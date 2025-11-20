de3761e78ac8979c15db187933b12cba3a1460d7
Jose Matos <silencioso0000@gmail.com>
2025-11-20 19:53:49 +0000

    cambios

--- variables.py
+++ variables.py
@@ -1,23 +1,23 @@
-import time
 import tkinter as tk 
 ventana = tk.Tk()
-ventana.title("ejemplo reloj")
+ventana.title("ejemplo boton")
 ventana.geometry("400x500")
 ventana.configure(bg='lightblue')
-
-
 
 etiqueta = tk.Label(ventana, text='hola soy un label')
 etiqueta.pack()
 
-def actualizar_hora():
-	etiqueta.config (text=time.strftime("14:14:15"))
-	etiqueta.after(1000,actualizar_hora)
-	etiqueta.config(fg='blue',bg='lightgreen', font=("Arial",16, 'bold'))
-actualizar_hora()
+boton = tk.Button(ventana, text='presiona aqui')
+boton.pack()
+
+def cambiar_testo():
+	print("!boton presionado")
+
+
+boton.config(command=cambiar_testo)
 
 
 
 
-
-
+ventana.mainloop()
+cambiar_testo()
