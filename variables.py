0d74592ea1c95cf10265360194d04dee57025bd2
Jose Matos <silencioso0000@gmail.com>
2025-11-20 19:51:19 +0000

    trabajando con ventanas

--- variables.py
+++ variables.py
@@ -1,12 +1,20 @@
+import time
+import tkinter as tk 
+ventana = tk.Tk()
+ventana.title("ejemplo reloj")
+ventana.geometry("400x500")
+ventana.configure(bg='lightblue')
 
 
-a=12
-b=12
-c=a=b
-print(c)
 
+etiqueta = tk.Label(ventana, text='hola soy un label')
+etiqueta.pack()
 
-
+def actualizar_hora():
+	etiqueta.config (text=time.strftime("14:14:15"))
+	etiqueta.after(1000,actualizar_hora)
+	etiqueta.config(fg='blue',bg='lightgreen', font=("Arial",16, 'bold'))
+actualizar_hora()
 
 
 
